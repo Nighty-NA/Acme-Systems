45d2b92a6f1801efb8671f9055942f6e32cea325
Nathaniel Arrowood <69820226+Nighty-NA@users.noreply.github.com>
2022-08-24 11:56:54 -0400

    Update HelloDolly.java

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -3,7 +3,8 @@ package ver1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
-		System.out.println("Print statement to main");
+		System.out.println("Print statement from main");
+		System.out.println("Hello Dolly from Eclipse Again");
 	}
 
 }
